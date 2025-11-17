7b8a43e42958646cd2830caa2d7d34ec69371b53
yingdemu <2458319912@qq.com>
2025-11-17 17:25:17 +0800

    Timer和Key的初始化

--- Hardware/Key.c
+++ Hardware/Key.c
@@ -1,49 +1,79 @@
 #include "stm32f10x.h"                  // Device header
-#include "Delay.h"
 
-/**
-  * 函    数：按键初始化
-  * 参    数：无
-  * 返 回 值：无
-  */
+
+//全局变量，用于存储按键键码
+uint8_t Key_Num;
+
 void Key_Init(void)
 {
-	/*开启时钟*/
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);		//开启GPIOB的时钟
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);	  //初始化A0 B0 B1
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);	  //代表 确定 上 下
 	
-	/*GPIO初始化*/
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);						//将PB1和PB11引脚初始化为上拉输入
+	GPIO_Init(GPIOA, &GPIO_InitStructure);		
+
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);					
+	
+	
 }
 
-/**
-  * 函    数：按键获取键码
-  * 参    数：无
-  * 返 回 值：按下按键的键码值，范围：0~2，返回0代表没有按键按下
-  * 注意事项：此函数是阻塞式操作，当按键按住不放时，函数会卡住，直到按键松手
-  */
-uint8_t Key_GetNum(void)
+uint8_t Key_GetNum(void)   //返回数字  0代表没有按下，1代表A0 2代表B0 3代表B1
 {
-	uint8_t KeyNum = 0;		//定义变量，默认键码值为0
-	
-	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0)			//读PB1输入寄存器的状态，如果为0，则代表按键1按下
+	uint8_t Temp;			
+	if (Key_Num)			
 	{
-		Delay_ms(20);											//延时消抖
-		while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0);	//等待按键松手
-		Delay_ms(20);											//延时消抖
-		KeyNum = 1;												//置键码为1
+		Temp = Key_Num;		
+		Key_Num = 0;		
+		return Temp;		
+	}
+	return 0;				
+}
+
+uint8_t Key_GetState(void)
+{
+	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0)		
+	{
+		return 1;		
 	}
 	
-	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11) == 0)			//读PB11输入寄存器的状态，如果为0，则代表按键2按下
+	
+	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_0) == 0)		
 	{
-		Delay_ms(20);											//延时消抖
-		while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11) == 0);	//等待按键松手
-		Delay_ms(20);											//延时消抖
-		KeyNum = 2;												//置键码为2
+		return 2;		
 	}
 	
-	return KeyNum;			//返回键码值，如果没有按键按下，所有if都不成立，则键码为默认值0
+	
+	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0)		
+	{
+		return 3;		
+	}
+
+	return 0;			
+}
+
+void Key_Tick(void)
+{
+	static uint8_t Count;					
+	static uint8_t CurrState, PrevState;	
+	
+	Count ++;			
+	if (Count >= 20)	
+	{
+		Count = 0;		
+		
+		PrevState = CurrState;			
+		CurrState = Key_GetState();		
+		
+		//如果本次状态的键码为0，且上次键码不为0，即检测到按键松手瞬间
+		if (CurrState == 0 && PrevState != 0)
+		{
+			Key_Num = PrevState;
+		}
+	}
 }
---
+++ Hardware/Timer.c
@@ -0,0 +1,29 @@
+#include "stm32f10x.h" 
+
+
+
+void Timer_Init(void)
+{
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3,ENABLE);
+	TIM_InternalClockConfig(TIM3);
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+	TIM_TimeBaseInitStruct.TIM_ClockDivision=TIM_CKD_DIV1;
+	TIM_TimeBaseInitStruct.TIM_CounterMode=TIM_CounterMode_Up;
+	TIM_TimeBaseInitStruct.TIM_Period=10000-1;
+	TIM_TimeBaseInitStruct.TIM_Prescaler=72-1;
+	TIM_TimeBaseInitStruct.TIM_RepetitionCounter=0;
+	TIM_TimeBaseInit(TIM3,&TIM_TimeBaseInitStruct);
+	
+	TIM_ITConfig(TIM3,TIM_IT_Update,ENABLE);
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel=TIM3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority=1;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	TIM_Cmd(TIM3,ENABLE);
+}
